0b4b52a960cbd57d7be38db25ff5c0a7f7b78c1d
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2025-06-10 16:26:02 -0500

    tab bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,6 @@
 <script setup lang="ts">
-import { Check } from 'lucide-vue-next'
 import { cn } from '@/lib/utils'
-import { Button } from '@/components/ui/button'
-import { Label } from '@/components/ui/label'
-import {
-  NumberField,
-  NumberFieldContent,
-  NumberFieldDecrement,
-  NumberFieldIncrement,
-  NumberFieldInput,
-} from '@/components/ui/number-field'
+import TabNav from '@/components/global/TabNav.vue'
 
 import {
   Card,
@@ -23,25 +14,16 @@ import {
 
 <template>
   <div class="flex items-center justify-center h-screen overflow-hidden w-screen">
-    <Card :class="cn('w-[380px]', $attrs.class ?? '')">
+    <Card :class="cn('w-[92vw] h-[92vh] py-0', $attrs.class ?? '')">
       <CardHeader>
         <CardTitle></CardTitle>
         <CardDescription></CardDescription>
       </CardHeader>
       <CardContent class="grid gap-4">
-        <NumberField>
-        <Label></Label>
-        <NumberFieldContent>
-          <NumberFieldDecrement />
-          <NumberFieldInput />
-          <NumberFieldIncrement />
-        </NumberFieldContent>
-      </NumberField>
+        <TabNav>
+        </TabNav>
       </CardContent>
       <CardFooter>
-        <Button class="w-full">
-          <Check class="mr-2 h-4 w-4" />
-        </Button>
       </CardFooter>
     </Card>
   </div>
---
+++ frontend/src/components/global/TabNav.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+    import { Tabs, TabsContent, TabsList, TabsTrigger } from '../ui/tabs'
+</script>
+
+<template>
+    <Tabs default-value="harvest" class = "w-full flex flex-col items-center justify-center">
+    <TabsList class = "m-auto">
+        <TabsTrigger value="locations">
+            Locations
+        </TabsTrigger>
+        <TabsTrigger value="plant-types">
+            Plant Types
+        </TabsTrigger>
+        <TabsTrigger value="plants">
+            Plants
+        </TabsTrigger>
+        <TabsTrigger value="harvest">
+            Harvest
+        </TabsTrigger>
+        <TabsTrigger value="bug-treatment">
+            Bug Treatment
+        </TabsTrigger>
+    </TabsList>
+
+        <TabsContent value="locations" class = "w-full">
+            <h2>Locations</h2>
+            <slot name="locations" />
+        </TabsContent>
+        <TabsContent value="plant-types" class = "w-full">
+            <h2>Plant Types</h2>
+            <slot name="plant-types" />
+        </TabsContent>
+        <TabsContent value="plants" class = "w-full">
+            <h2>Plants</h2>
+            <slot name="plants" />
+        </TabsContent>
+        <TabsContent value="harvest" class = "w-full">
+            <h2>Harvest</h2>
+            <slot name="harvest" />
+        </TabsContent>
+        <TabsContent value="bug-treatment" class = "w-full">
+            <h2>Bug Treatment</h2>
+            <slot name="bug-treatment" />
+        </TabsContent>
+    </Tabs>
+
+</template>
